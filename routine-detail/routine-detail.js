99c9b68963ab2cc91c0f18496c67a0bdc0a99858
Alice Hsing <alicehsing@gmail.com>
2022-01-20 17:32:07 -0800

    added event listener to each exercise so on click a video is rendered

--- routine-detail/routine-detail.js
+++ routine-detail/routine-detail.js
@@ -1,4 +1,4 @@
-import { checkAuth, getOneRoutineAndExercises, logout } from '../fetch-utils.js';
+import { checkAuth, getExerciseVideo, getOneRoutineAndExercises, logout, } from '../fetch-utils.js';
 import { renderExercises } from '../render-utils.js';
 
 checkAuth();
@@ -7,7 +7,7 @@ const routineNameEl = document.querySelector('.routine-name');
 const durationEl = document.querySelector('.duration');
 const exerciseListEl = document.querySelector('.exercises-list');
 const startWorkoutBtnEl = document.querySelector('.start-workout');
-
+const videoDisplayEl = document.querySelector('.video-display');
 const logoutButton = document.getElementById('logout');
 
 logoutButton.addEventListener('click', () => {
@@ -25,7 +25,7 @@ window.addEventListener('load', async() => {
 
     const routineName = getRoutine[0].routines.name;
     routineNameEl.textContent = `${routineName}`;
-
+    //display total duration
     for (let duration of getRoutine[0].routines.exercises) {
         arr.push(duration.duration);
         let sum = 0;
@@ -37,18 +37,22 @@ window.addEventListener('load', async() => {
     }
     
     for (const exercise of getRoutine[0].routines.exercises) {
-        const exerciseWorkout = renderExercises(exercise);
-
+        const exerciseWorkout = renderExercises(exercise);   
         exerciseListEl.append(exerciseWorkout);
+
+        //when user clicks a display exercise
+        exerciseWorkout.addEventListener('click', async() => {
+        //fetch video links in supabase 'exercises' table
+            const exerciseVideo = await getExerciseVideo(exercise.id);
+
+            const videoDiv = document.createElement('iframe');
+            videoDiv.classList.add('exercise-video') ;
+            videoDiv.src = exerciseVideo.video_link;
+            videoDiv.title = "YouTube video player";
+            videoDisplayEl.append(videoDiv);
+        });
     }
 });
-
-
-startWorkoutBtnEl.addEventListener('click', () => {
-    window.location.href = `../timer/?id=${id}`;
-
-});
-
 
 function convertHMS(value) {
     const sec = parseInt(value, 10); // convert value to number if it's string
@@ -61,3 +65,20 @@ function convertHMS(value) {
     if (seconds < 10) {seconds = '0' + seconds;}
     return minutes + ':' + seconds; // Return is HH : MM : SS
 }
+
+// async function displayExerciseVideo() {
+//     const getRoutine = await getOneRoutineAndExercises();
+//     const routineName = getRoutine[0].routines.name;
+//     routineNameEl.textContent = `${routineName}`;
+
+//     for (const exercise of getRoutine[0].routines.exercises) {
+//         const exerciseWorkout = renderExercises(exercise);   
+//         exerciseListEl.append(exerciseWorkout);
+//     }
+    
+    
+// }
+startWorkoutBtnEl.addEventListener('click', () => {
+    window.location.href = `../timer/?id=${id}`;
+
+});
